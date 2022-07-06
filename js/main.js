563dceff1de76d5137d6eac9d434389f9a1381c9
lupeMorales <morales.lupegmail.com>
2022-07-06 09:27:52 +0200

    add show count

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ const inputNumber = document.querySelector(".js-inputNum");
 const button = document.querySelector(".js-btn");
 const clue = document.querySelector(".js-clue");
 const count = document.querySelector(".js-count");
+
+let acc = 0;
+function showCount() {
+  acc = acc + 1;
+  count.innerHTML = `Números de intentos: ${acc}`;
+  console.log(acc);
+}
+
+function handleClick(ev) {
+  ev.preventDefault();
+  showCount();
+  console.log("click button");
+}
+
+button.addEventListener("click", handleClick);
