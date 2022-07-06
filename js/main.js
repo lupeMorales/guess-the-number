0430028c0236c74e192c54ddd289b6879ac68b97
lupeMorales <morales.lupegmail.com>
2022-07-06 09:04:20 +0200

    select DOM elements

---
+++ js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const inputNumber = document.querySelector(".js-inputNum");
+const button = document.querySelector(".js-btn");
+const clue = document.querySelector(".js-clue");
+const count = document.querySelector(".js-count");
